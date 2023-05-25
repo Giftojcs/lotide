3f577bcc2ac4645f3814af4fd205c30aae632f18
giftojcs <labber@DESKTOP-ILVQTOL>
2023-05-24 22:17:31 -0600

    countOnly.js

---
+++ countOnly.js
@@ -0,0 +1,50 @@
+const assertEqual = function(actual, expected) {
+  if (actual === expected) {
+    console.log(`✅✅✅ Assertion Passed: ${actual} === ${expected}`);
+  } else {
+    console.log(`🛑🛑🛑 Assertion Failed: ${actual} !== ${expected}`);
+  }
+};
+
+const countOnly = function(names, itemsToCount) {
+  const results = {};
+
+  for (const name of names) {
+    if (itemsToCount[name]) {
+      if (results[name]) {
+        results[name] += 1;
+      } else {
+        results[name] = 1;
+      }
+    }
+  }
+
+  return results;
+};
+
+// Test cases for assertEqual
+assertEqual("Hello", "Hello"); // Identical strings (Pass)
+assertEqual("Hello", "hello"); // Non-identical strings (Fail)
+assertEqual(5, 5); // Identical numbers (Pass)
+assertEqual(10, 20); // Non-identical numbers (Fail)
+
+// Test case for countOnly
+const firstNames = [
+  "Karl",
+  "Salima",
+  "Agouhanna",
+  "Fang",
+  "Kavith",
+  "Jason",
+  "Salima",
+  "Fang",
+  "Joe"
+];
+
+const result1 = countOnly(firstNames, { "Jason": true, "Karima": true, "Fang": true, "Agouhanna": false });
+
+assertEqual(result1["Jason"], 1);
+assertEqual(result1["Karima"], undefined);
+assertEqual(result1["Fang"], 2);
+assertEqual(result1["Agouhanna"], undefined);
+
