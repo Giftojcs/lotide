15aa7cc24a25f5bb64f60c58ca5a0cdabe3fb573
giftojcs <labber@DESKTOP-ILVQTOL>
2023-05-25 08:54:01 -0600

    countLetters.js

---
+++ countLetters.js
@@ -0,0 +1,37 @@
+const assertEqual = function(actual, expected) {
+  if (actual === expected) {
+    console.log(`✅✅✅ Assertion Passed: ${actual} === ${expected}`);
+  } else {
+    console.log(`🛑🛑🛑 Assertion Failed: ${actual} !== ${expected}`);
+  }
+};
+
+// Test cases
+assertEqual("Hello", "Hello"); // Identical strings (Pass)
+assertEqual("Hello", "hello"); // Non-identical strings (Fail)
+assertEqual(5, 5); // Identical numbers (Pass)
+assertEqual(10, 20); // Non-identical numbers (Fail)
+
+
+//Creating a function called counted counterLetter using Lighthouselab.
+
+const countLetters = function(sentence) {
+  const letterCounts = {};
+
+  for (const letter of sentence) {
+    if (letter !== ' ') {
+      if (letterCounts[letter]) {
+        letterCounts[letter] += 1;
+      } else {
+        letterCounts[letter] = 1;
+      }
+    }
+  }
+
+  return letterCounts;
+};
+
+// Test case
+const result = countLetters('Lighthouselab');
+console.log(result);
+
