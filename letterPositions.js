dda5a1e5a7a8d3de4db94685b26e91b8e5b342c3
giftojcs <labber@DESKTOP-ILVQTOL>
2023-05-26 13:15:48 -0600

    letterPositions.js

---
+++ letterPositions.js
@@ -0,0 +1,44 @@
+const eqArrays = function(arr1, arr2) {
+  if (arr1.length !== arr2.length) {
+    return false;
+  }
+
+  for (let i = 0; i < arr1.length; i++) {
+    if (arr1[i] !== arr2[i]) {
+      return false;
+    }
+  }
+
+  return true;
+};
+
+const assertArraysEqual = function(actual, expected) {
+  if (eqArrays(actual, expected)) {
+    console.log(`✅ Assertion Passed: ${actual} === ${expected}`);
+  } else {
+    console.log(`❌ Assertion Failed: ${actual} !== ${expected}`);
+  }
+};
+
+const letterPositions = function(sentence) {
+  const results = {};
+
+  for (let i = 0; i < sentence.length; i++) {
+    const letter = sentence[i].toLowerCase();
+
+    if (letter !== ' ') {
+      if (results[letter]) {
+        results[letter].push(i);
+      } else {
+        results[letter] = [i];
+      }
+    }
+  }
+
+  return results;
+};
+
+// Test case
+const positions = letterPositions("hello");
+assertArraysEqual(positions['e'], [1]); // Expect assertion to pass
+
