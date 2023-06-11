36e9c5bc3c4c3ff19bdc47eedc49deaae48abc55
giftojcs <ojaighog@gmail.com>
2023-06-10 22:27:55 -0600

    test

--- test/eqArraysTest.js
+++ test/eqArraysTest.js
@@ -6,3 +6,4 @@ assertEqual(eqArrays([1, 2, 3], [1, 2, 3]), true); // Identical arrays (Pass)
 assertEqual(eqArrays([1, 2, 3], [3, 2, 1]), false); // Non-identical arrays (Fail)
 assertEqual(eqArrays(["1", "2", "3"], ["1", "2", "3"]), true); // Identical arrays (Pass)
 assertEqual(eqArrays(["1", "2", "3"], ["1", "2", 3]), false); // Non-identical arrays (Fail)
+
