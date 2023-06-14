8f2433d9e4636aed8c63dd5f109a93cd811adf73
giftojcs <ojaighog@gmail.com>
2023-06-13 21:42:57 -0600

    Adding assertArraysequal.js file

--- assertArraysEqual.js
+++ assertArraysEqual.js
@@ -1,33 +1,12 @@
-const eqArrays = function(arr1, arr2) {
-  // If arrays have different lengths, they can't be a perfect match
-  if (arr1.length !== arr2.length) {
-    return false;
-  }
-
-  // Iterate over each element in the arrays
-  for (let i = 0; i < arr1.length; i++) {
-    // If any corresponding elements are not equal, arrays are not a perfect match
-    if (arr1[i] !== arr2[i]) {
-      return false;
-    }
-  }
-
-  // All elements are equal, arrays are a perfect match
-  return true;
-};
+const eqArrays = require('./eqArrays');
 
 const assertArraysEqual = function(arr1, arr2) {
-  const arraysEqual = eqArrays(arr1, arr2);
-  if (arraysEqual) {
-    console.log(`✅✅✅ Assertion Passed: Arrays are equal.`);
+  if (eqArrays(arr1, arr2)) {
+    console.log(`✅✅✅ Assertion Passed: [${arr1}] === [${arr2}]`);
   } else {
-    console.log(`🛑🛑🛑 Assertion Failed: Arrays are not equal.`);
+    console.log(`🛑🛑🛑 Assertion Failed: [${arr1}] !== [${arr2}]`);
   }
 };
 
-// Test cases
-assertArraysEqual([1, 2, 3], [1, 2, 3]); // Identical arrays (Pass)
-assertArraysEqual([1, 2, 3], [3, 2, 1]); // Non-identical arrays (Fail)
-assertArraysEqual(["1", "2", "3"], ["1", "2", "3"]); // Identical arrays (Pass)
-assertArraysEqual(["1", "2", "3"], ["1", "2", 3]); // Non-identical arrays (Fail)
+module.exports = assertArraysEqual;
 
