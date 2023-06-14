d4feeabb860a3d4a400b8e181dd8fc70d8947860
giftojcs <ojaighog@gmail.com>
2023-06-13 21:44:15 -0600

    Adding assertEqual.js file

--- assertEqual.js
+++ assertEqual.js
@@ -7,8 +7,6 @@ const assertEqual = function(actual, expected) {
 };
 
 // Test cases
-assertEqual("Hello", "Hello"); // Identical strings (Pass)
-assertEqual("Hello", "hello"); // Non-identical strings (Fail)
-assertEqual(5, 5); // Identical numbers (Pass)
-assertEqual(10, 20); // Non-identical numbers (Fail)
+
+module.exports = assertEqual;
 
